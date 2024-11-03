cd23fb64630bb2dc95340e3731c688d4091554bf
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-11-03 19:18:38 +0600

    express setup

---
+++ src/index.ts
@@ -0,0 +1,50 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import { Server } from 'http';
+import config from './app/config';
+
+const app = express();
+let server: Server;
+
+// parser
+app.use(express.json());
+app.use(cors());
+
+// test endpoint
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World!');
+});
+
+// not found route
+app.all('*', (req: Request, res: Response) => {
+    res.status(404).json({
+        success: false,
+        statusCode: 404,
+        message: "Endpoint Not Found",
+    });
+});
+
+server = app.listen(config.port || 5000, () => {
+    console.log(`MedPro is listening on port ${config.port || 5000}`);
+});
+
+// async error handle
+process.on('unhandledRejection', () => {
+    console.log('unhandledRejection is detected! shutting down the server...');
+
+    if (server) {
+        server.close(() => {
+            process.exit(1);
+
+        });
+    };
+
+    process.exit(1);
+});
+
+
+// synchronies error handle
+process.on('uncaughtException', () => {
+    console.log('uncaughtException is detected! shutting down the server...');
+    process.exit();
+});
