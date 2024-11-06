59b9747b6ec2308153ec7fa5d0c44e5ff76d90dc
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-11-06 17:31:49 +0600

    all operation done for fetching single admin by his/har id

--- src/app/modules/users/admin/admin.controllers.ts
+++ src/app/modules/users/admin/admin.controllers.ts
@@ -7,7 +7,7 @@ async function getAllAdmin(req: Request, res: Response) {
         res.status(result.statusCode).json(result);
 
     } catch (error) {
-        res.send(500).json({
+        res.status(500).json({
             success: false,
             message: 'something went wrong',
             error
@@ -18,6 +18,7 @@ async function getAllAdmin(req: Request, res: Response) {
 async function getSingleAdminByID(req: Request, res: Response) {
     try {
         const result = await AdminServices.getSingleAdminByIDFromDb(req.params.id);
+        res.status(result.statusCode).json(result);
 
     } catch (error) {
         res.send(500).json({
