35a68128d887be205f96ded935bdcd87602da298
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-11-07 11:59:09 +0600

    Admin info updated successfully by id

--- src/app/modules/users/admin/admin.routes.ts
+++ src/app/modules/users/admin/admin.routes.ts
@@ -5,5 +5,6 @@ const router = Router();
 
 router.get('/', AdminControllers.getAllAdmin);
 router.get('/:id', AdminControllers.getSingleAdminByID);
+router.patch('/:id', AdminControllers.updateAdmin)
 
 export const AdminRoutes = router;
