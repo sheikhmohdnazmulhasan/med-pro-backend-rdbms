0c2a1603a8a8e2d11e4477c24508f21137fd678f
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-11-09 11:31:20 +0600

    logged in with access token and refresh token for cookie

--- src/app/modules/auth/auth.controllers.ts
+++ src/app/modules/auth/auth.controllers.ts
@@ -4,8 +4,20 @@ import { AuthServices } from "./auth.services";
 async function login(req: Request, res: Response) {
     try {
         const result = await AuthServices.loginFromDb(req.body);
+        // @ts-ignore: refresh token is always exist
+        const { refreshToken, ...dataWithRefreshToken } = result.data;
 
-        // send response
+        const responseWithRefreshToken = {
+            ...result,
+            data: dataWithRefreshToken
+        };
+
+        res.cookie('refreshToken', refreshToken, {
+            secure: false,
+            httpOnly: true
+        });
+
+        res.status(result.statusCode).json(responseWithRefreshToken);
 
     } catch (error: any) {
         res.send(500).json({
