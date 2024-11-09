4bea0d317e507ad7ca57cf98a3e7e9e314bf660b
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-11-09 10:28:38 +0600

    verifying user password

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -5,5 +5,6 @@ dotenv.config({ path: path.join(process.cwd(), '.env') });
 
 export default {
     port: process.env.PORT,
-    bcrypt_salt_round: process.env.BCRYPT_SALT_ROUND
+    bcrypt_salt_round: process.env.BCRYPT_SALT_ROUND,
+    jwt_access_token_secret: process.env.JWT_ACCESS_TOKEN_SECRET
 }
--- src/app/modules/auth/auth.services.ts
+++ src/app/modules/auth/auth.services.ts
@@ -1,9 +1,28 @@
+import bcrypt from 'bcryptjs';
+import { prisma } from '../../constants/prisma_constructor';
+
 async function loginFromDb(payload: {
     email: string;
     password: string;
 }) {
     try {
-        console.log(payload);
+        const user = await prisma.user.findUniqueOrThrow({
+            where: {
+                email: payload.email
+            }
+        });
+
+        const isCorrectPassword = await bcrypt.compare(payload.password, user.password);
+
+        if (!isCorrectPassword) {
+            return {
+                success: false,
+                statusCode: 401,
+                message: 'Incorrect password'
+            };
+        };
+
+
 
     } catch (error: any) {
         return {
