fa777673fc6e17b434ee5213de543c0a9375f49f
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-11-15 12:03:33 +0600

    unhandledRejection error fixed in auth controller

--- src/app/modules/auth/auth.controllers.ts
+++ src/app/modules/auth/auth.controllers.ts
@@ -4,20 +4,26 @@ import { AuthServices } from "./auth.services";
 async function login(req: Request, res: Response) {
     try {
         const result = await AuthServices.loginFromDb(req.body);
-        // @ts-ignore: refresh token is always exist
-        const { refreshToken, ...dataWithRefreshToken } = result.data;
 
-        const responseWithRefreshToken = {
-            ...result,
-            data: dataWithRefreshToken
-        };
+        if (result.success) {
+            // @ts-ignore: refresh token is always exist
+            const { refreshToken, ...dataWithoutRefreshToken } = result.data;
 
-        res.cookie('refreshToken', refreshToken, {
-            secure: false,
-            httpOnly: true
-        });
+            const responseWithRefreshToken = {
+                ...result,
+                data: dataWithoutRefreshToken
+            };
 
-        res.status(result.statusCode).json(responseWithRefreshToken);
+            res.cookie('refreshToken', refreshToken, {
+                secure: false,
+                httpOnly: true
+            });
+
+            res.status(result.statusCode).json(responseWithRefreshToken);
+
+        } else {
+            res.status(result.statusCode).json(result);
+        }
 
     } catch (error: any) {
         res.send(500).json({
