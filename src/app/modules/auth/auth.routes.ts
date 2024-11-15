e97a74b1fe598612161589ef78da7a07f6c90ea7
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-11-15 22:28:39 +0600

    password changed successfully

--- src/app/modules/auth/auth.routes.ts
+++ src/app/modules/auth/auth.routes.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
 import { authControllers } from "./auth.controllers";
+import Auth from "../../middlewares/auth";
+import { UserRole } from "@prisma/client";
 
 const router = Router();
 
 router.post('/login', authControllers.login);
+
+router.patch('/change-password',
+    Auth([UserRole.ADMIN, UserRole.DOCTOR, UserRole.PATIENT, UserRole.SUPER_ADMIN]),
+    authControllers.changePassword)
 
 export const AuthRoutes = router;
