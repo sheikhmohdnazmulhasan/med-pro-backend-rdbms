40a2916e5599aff3da95e159cc8d134738100bd0
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-11-16 20:19:09 +0600

    multer disk storage created

--- src/app/modules/users/user.routes.ts
+++ src/app/modules/users/user.routes.ts
@@ -2,9 +2,25 @@ import { Router } from "express";
 import { UserControllers } from "./user.controllers";
 import Auth from "../../middlewares/auth";
 import { UserRole } from "@prisma/client";
+import multer from 'multer';
+import path from "path";
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, path.join(process.cwd(), 'uploads'))
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+    }
+})
+
+const upload = multer({ storage: storage })
 
 const router = Router();
-
-router.post('/create-admin', Auth([UserRole.ADMIN, UserRole.SUPER_ADMIN]), UserControllers.createAdmin);
+router.post('/create-admin',
+    Auth([UserRole.ADMIN, UserRole.SUPER_ADMIN]),
+    upload.single('file'),
+    UserControllers.createAdmin);
 
 export const UserRoutes = router
